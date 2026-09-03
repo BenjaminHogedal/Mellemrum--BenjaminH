a1ad06eb6598d3bb4064a787ef777ea6a78c5326
BenjaminHogedal <eaa25bvh@students.eaaa.dk>
2026-09-03 15:14:50 +0200

    Tilføj scroll til toppen ved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import EventPage from "./pages/EventPage";
 import RegistrationsPage from "./pages/RegistrationsPage";
-import AdminEventsPage from "./pages/AdminEventsPage"
+import AdminEventsPage from "./pages/AdminEventsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
